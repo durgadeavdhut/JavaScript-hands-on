3e21078e1c93fbd33672ada5bdbdccf397fad679
Avdhut Durgade <131164950+durgadeavdhut@users.noreply.github.com>
2025-07-29 10:03:38 +0530

    variable
    variable

---
+++ 01-variable.js
@@ -0,0 +1,27 @@
+//variable declaration
+
+var firstName;
+console.log("firstName Variable declaration:"+firstName);
+
+firstName="Elon"; //Variable initialization
+console.log("firstName-Variable initialization:"+firstName);
+
+firstName="Stew"; // variable update 
+console.log("firstName-variable update:"+firstName);
+
+firstName="Avdhut"; // variable update 
+console.log("firstName-variable update:"+firstName);
+
+firstName="Somesh"; // variable update 
+console.log("firstName-variable update:"+firstName);
+
+console.log("==========variable declaration and initialization========");
+var city="pune";
+console.log( "city is :"+ city);
+
+var collegeName ="Sharadchandra Pawar College of Engineering and Technology, Baramati";
+console.log("My College Name is:"+ collegeName);
+
+
+
+
---
+++ dist/01-variable-assignment.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// //Assignment
+// console.log("========1========");
+// var fullName;
+// fullName ="Avdhut Durgade";
+// console.log("My full name is : "+fullName );
+// fullName = "Avdhut Mohan Durgade";
+// console.log("Updated Full name is : "+ fullName);
+// console.log("========2========");
+// var myLove ="I Love only JS";
+// console.log(myLove);
+// console.log("========3========");
+// var firstName="Avdhut";
+// console.log("First Name:"+firstName);
+// var lastName="Durgade";
+// console.log("Last name:"+lastName);
+// var age=22;
+// console.log("Age:"+age);
+// var address="Walhe,Pune"; 
+// console.log("Address:"+ address);
+// var pinCode=412305; 
+// console.log("Pincode:"+pinCode);
+// var gender="Male";
+// console.log("Gender:"+gender);
+// var isMarried="Unmarried";
+// console.log("Married Status:"+isMarried);
+//Assignment
+var city = "Pune";
+console.log("City is : " + city);
+var bestfriend = "Aditya";
+console.log("BestFriend is : " + bestfriend);
+var country = "India";
+console.log("Country is : " + country);
+var school = "Maharshi Walmiki Vidyalaya, Walhe";
+console.log("School Name is : " + school);
+var collegeName = "Sharadchandra Pawar College of Engineering and Technology, Baramati";
+console.log("College Name is : " + collegeName);

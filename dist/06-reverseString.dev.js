af80f2620590ea7146161f62c39a9486c0f8851c
Avdhut Durgade <131164950+durgadeavdhut@users.noreply.github.com>
2025-08-08 11:40:00 +0530

    ok
    ok

--- dist/06-reverseString.dev.js
+++ dist/06-reverseString.dev.js
@@ -1,26 +1,21 @@
 "use strict";
 
-// Function to reverse string ignoring spaces
 function reverseStringIgnoreSpaces(str) {
-  var result = ""; // To store reversed string without spaces
-  // Loop from the end of the string to the start
+  var result = "";
 
   for (var i = str.length - 1; i >= 0; i--) {
-    var ch = str.charAt(i); // Get character at position i
+    var ch = str.charAt(i);
 
     if (ch !== ' ') {
-      // Skip spaces
-      result += ch; // Add non-space characters
+      result += ch;
     }
   }
 
-  return result; // Return reversed string without spaces
-} // Test strings for Assignment 2
-
+  return result;
+}
 
 var s1 = "Hard work always pays back";
-var s2 = "Soon I will be Angular IT Champ"; // Invoke function and log output
-
+var s2 = "Soon I will be Angular IT Champ";
 console.log("========== Reversed String ==========");
 console.log("String 1: \"".concat(s1, "\""));
 console.log("Reversed String is :", reverseStringIgnoreSpaces(s1));
